87e03e8b4ccb96b8621cb6a79a4e89e556b6d3cb
lkoosss <lkoosss@gmail.com>
2022-06-22 21:11:18 +0900

    Project Frame create

---
+++ Springboot/frame/src/main/java/com/example/frame/FrameApplication.java
@@ -0,0 +1,13 @@
+package com.example.frame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameApplication.class, args);
+    }
+
+}
