6489d08abd8885d3b519e544f87e87475d55f927
ssoh <lkoosss@gmail.com>
2023-06-05 21:14:18 +0900

    Tus 재개 가능한 업로드 완료

--- Springboot/frame/src/main/webapp/WEB-INF/views/tus/tusMain.jsp
+++ Springboot/frame/src/main/webapp/WEB-INF/views/tus/tusMain.jsp
@@ -14,8 +14,7 @@
     <link type="text/css" rel="stylesheet" href="/css/views/tus/tusMain.css">
 
     <!--  Script  -->
-<%--    <script type="text/javascript" src="/js/lib/jquery.min_1.12.4.js" defer></script>--%>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/lib/jquery.min_1.12.4.js" defer></script>
     <script type="text/javascript" src="/js/lib/tus/tus.min.js" defer></script>
     <script type="text/javascript" src="/js/views/tus/tusMain.js" defer></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -23,25 +22,13 @@
 </head>
 <body>
     <h1>TUS Test</h1>
-    <div class="upload-card">
-        <div class="row">
-            <div class="col-md-12">
-                <div id="input-section">
-                    <div class="heading">업로드할 파일을 선택해주세요:</div>
-                    <input type="file" id="js-file-input" multiple>
-                </div>
+    <div class="eddy-div">
+        <button id="downloadBtn">다운로드</button>
+        <button id="uploadBtn">업로드</button>
+        <input type="file" id="fileInput" multiple>
+        <div id="dataList">
 
-                <div id="progress-section">
-                    <div class="heading" id="heading"></div>
-                    <div class="d-flex">
-                        <div class="js-action-btn">
-                            <button class="btn" id="toggle-btn">파일 업로드</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
-    <div id="data-pre"></div>
 </body>
 </html>
--- Springboot/frame/src/main/webapp/js/views/tus/tusMain.js
+++ Springboot/frame/src/main/webapp/js/views/tus/tusMain.js
@@ -1,139 +1,115 @@
-const fileInput = document.querySelector('#js-file-input')
-const dataDiv = document.querySelector('#data-pre');
-const toggleBtn = document.querySelector('#toggle-btn')
+var tusMain = {
+    convertedFiles: [],
+}
+function init() {
+    addEvent();
+}
 
-function reset() {
-    convertedFiles.map((i, key) => {
+function addEvent() {
+
+    // 다운로드 버튼 클릭 이벤트
+    $("#downloadBtn").click( () => {
+        console.log("downloadBtn Click");
+        $.ajax({
+            url: "/tusMain/download",
+            type: "POST",
+            success: (result) => {
+                console.log(result);
+            }
+        })
+    })
+
+    // 파일선택 이벤트
+    $('#fileInput').change( (e) => {
+        $('#dataList').empty();
+
+        const files = e.target.files;
+        $.each(files, (index, item) => {
+            tusMain.convertedFiles = [
+                ...tusMain.convertedFiles,
+                {id: URL.createObjectURL(item), file: item}
+            ]
+        })
+
+        tusMain.convertedFiles.map( (index, key) => {
+            const file = index.file;
+            key ++;
+            const chunkSize = parseInt(104857600, 10);
+
+            var uploadFileTag = `    <div class="file-data" key=${key}>
+                                                <div>파일명: ${index.file.name}</div>
+                                                <div>파일 타입: ${index.file.type}</div>
+                                                <div>파일 크기: ${index.file.size} byte</div>
+                                                <div class="flex-grow-1">
+                                                  <div class="progress pr_${key}">
+                                                    <div class="progress-bar_${key} progress-bar-striped bg-success" role="progressbar"></div>
+                                                  </div>
+                                                  <div class="upload-text-progress" id="js-upload-text-progress_${key}"></div>
+                                                </div>
+                                              </div>`;
+            $('#dataList').append(uploadFileTag);
+
+            var uploadConfig = {
+                endpoint : "/tusMain/upload",
+                retryDelays: [0, 1000, 3000, 5000],
+                metadata: {
+                    filename: file.name,
+                    filetype: file.type
+                },
+                chunkSize,
+                onProgress: (bytesUploaded, bytesTotal) => {
+                    const percentage = ( (bytesUploaded / bytesTotal) * 100 ).toFixed(2);
+                    $('.progress-bar_' + key).css('width', percentage + '%');
+
+                    if (percentage < 100.00) {
+                        $('#js-upload-text-progress_' + key).html(`Uploaded ${getByteFormat(bytesUploaded)} of ${getByteFormat(bytesTotal)} (${percentage}%)`);
+                    } else {
+                        $('#js-upload-text-progress_' + key).html(`Uploaded ${getByteFormat(bytesUploaded)} of ${getByteFormat(bytesTotal)} (${percentage}%) 파일 저장 중 `);
+                    }
+                },
+                onSuccess: () => {
+                    $('#js-upload-text-progress_' + key).html("파일 저장 완료");
+                },
+                onError: (error) => {
+                    console.error("failed because: " + error);
+                }
+            }
+            var upload = new tus.Upload(file, uploadConfig);
+
+            // 업로드 클릭 이벤트
+            $('#uploadBtn').click( (e) => {
+                resetProgress();
+
+                upload.findPreviousUploads().then( (previousUploads) => {
+                    if (previousUploads.length) {
+                        upload.resumeFromPreviousUpload(previousUploads[0]);
+                    }
+
+                    upload.start();
+                })
+            })
+        })
+    })
+}
+
+function resetProgress() {
+    tusMain.convertedFiles.map( (i, key) => {
         $('.progress-bar_' + key).css('width', '0%');
         $('#js-upload-text-progress_' + key).html('');
     })
 }
-/**
- * Turn a byte number into a human readable format.
- * Taken from https://stackoverflow.com/a/18650828
- */
-function formatBytes(bytes, decimals = 2) {
+
+function getByteFormat(bytes, decimals = 2) {
     if (bytes === 0) {
-        return '0 Bytes'
+        return '0 Bytes';
     }
 
-    const k = 1024
-    const dm = decimals < 0 ? 0 : decimals
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const Unit = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
     const i = Math.floor(Math.log(bytes) / Math.log(k))
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
+
+    return parseFloat( (bytes / Math.pow(k, i)).toFixed(dm) ) + ' ' + Unit[i];
 }
 
-let convertedFiles = [];
-
-fileInput.addEventListener('change', (e) => {
-    dataDiv.innerHTML = '';
-    const files = e.target.files;
-
-    Object.keys(files).forEach(k => {
-        convertedFiles = [
-            ...convertedFiles,
-            { id: URL.createObjectURL(files[k]), file: files[k]}
-        ];
-    });
-
-    convertedFiles.map((i, key) => {
-        const file = i.file;
-        key ++
-        // 10MB
-        const chunkSize = parseInt(104857600, 10)
-        console.log(chunkSize);
-
-        dataDiv.innerHTML +=
-            `
-                      <div class="file-data" key=${key}>
-<!--                        <img alt="what" src=${i.id} width="150" />-->
-                        <div>파일명: ${i.file.name}</div>
-                        <div>파일 타입: ${i.file.type}</div>
-                        <div>파일 크기: ${i.file.size} byte</div>
-                        <div class="flex-grow-1">
-                          <div class="progress pr_${key}">
-                            <div class="progress-bar_${key} progress-bar-striped bg-success" role="progressbar"></div>
-                          </div>
-                          <div class="upload-text-progress" id="js-upload-text-progress_${key}"></div>
-                        </div>
-                      </div>
-                    `
-
-        let upload = new tus.Upload(file, {
-            endpoint: "/tusMain/upload",
-            chunkSize,
-            retryDelays: [0, 1000, 3000, 5000],
-            metadata: {
-                filename: file.name,
-                filetype: file.type
-            },
-            onError: function (error) {
-                console.log("Failed because: " + error);
-            },
-            onProgress: function (bytesUploaded, bytesTotal) {
-                const percentage = ((bytesUploaded / bytesTotal) * 100).toFixed(2)
-                $('.progress-bar_' + key).css('width', percentage + '%');
-
-                if (percentage < 100.00) {
-                    $('#js-upload-text-progress_' + key).html(`Uploaded ${formatBytes(bytesUploaded)} of ${formatBytes(bytesTotal)} (${percentage}%)`);
-                } else {
-                    $('#js-upload-text-progress_' + key).html(`Uploaded ${formatBytes(bytesUploaded)} of ${formatBytes(bytesTotal)} (${percentage}%) 파일 저장 중`);
-                }
-            },
-            onSuccess: function () {
-                $('#js-upload-text-progress_' + key).html("파일 저장 완료");
-            }
-        });
-
-        toggleBtn.addEventListener('click', (e) => {
-            reset();
-
-            // Check if there are any previous uploads to continue.
-            upload.findPreviousUploads().then(function (previousUploads) {
-                // Found previous uploads so we select the first one.
-                if (previousUploads.length) {
-                    upload.resumeFromPreviousUpload(previousUploads[0])
-                }
-
-                // Start the upload
-                upload.start()
-            })
-        });
-    });
-});
-
-// function init() {
-//     addEvent();
-// }
-//
-// function formatBytes(bytes, decimals = 2) {
-//     if (bytes === 0) {
-//         return '0 Bytes';
-//     }
-//
-//     const k = 1024;
-//     const dm = decimals < 0 ? 0 : decimals;
-//     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-//     const i = Math.floor(Math.log(bytes) / Math.log(k))
-//     return parseFloat( (bytes / Math.pow(k, i)).toFixed(dm) ) + ' ' + sizes[i];
-// }
-//
-// function addEvent() {
-//
-//     $('#fileInput').change( () => {
-//         console.log('change');
-//     })
-//
-//     $("#downloadBtn").click( () => {
-//         $.ajax({
-//             url: "/tusMain/download",
-//             type: "POST",
-//             success: (result) => {
-//                 console.log(result);
-//             }
-//         })
-//     })
-// }
-//
-// $(init)
+$(init)
